de110a2e444d30a3d95ad7cc28bb7a2013dcfc1b
anushree <anushreeee21@gmail.com>
2026-01-29 12:27:49 +0530

    palindrome

---
+++ Control-Statement/loops/NumberPalindrome.java
@@ -0,0 +1,19 @@
+public class NumberPalindrome {
+    public static void main(String[] args) {
+        int num = 12321;  // Change this number to test
+        int originalNum = num;
+        int reversed = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+
+        if (originalNum == reversed) {
+            System.out.println(originalNum + " is a palindrome.");
+        } else {
+            System.out.println(originalNum + " is not a palindrome.");
+        }
+    }
+}
