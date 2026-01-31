2831d9875fb9bc949b21536094be1d0ca102b77e
anushree <anushreeee21@gmail.com>
2026-01-31 20:56:26 +0530

    Bubble Sort

---
+++ Control-Statement/practice/New folder/sorting/bubblesort.java
@@ -0,0 +1,32 @@
+
+
+class bubblesort {
+    void bubbleSort(int arr[])
+    {
+        int n = arr.length;
+      
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr[j] > arr[j + 1]) {
+                  
+                    // swap temp and arr[i]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+    }
+    public static void main(String args[])
+    {
+        bubblesort ob = new bubblesort();
+
+        int a[] = { 64, 34, 25, 12 };
+      
+        ob.bubbleSort(a);
+      
+      	int n = a.length;
+      
+        for (int i = 0; i < n; ++i)
+            System.out.print(a[i] + " ");
+        System.out.println();
+    }
+}
