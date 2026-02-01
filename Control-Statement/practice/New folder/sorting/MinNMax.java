a8bdb7509e6b4128dfeb7b4168dabf9bc5c7917a
anushree <anushreeee21@gmail.com>
2026-02-01 12:46:17 +0530

    Min n max value

---
+++ Control-Statement/practice/New folder/sorting/MinNMax.java
@@ -0,0 +1,18 @@
+
+
+public class MinNMax {
+    public static void main(String[] args) {
+
+        int[] num = {8, 3, 6, 1, 9};
+
+        int min = num[0];
+        int max = num[0];
+        for (int i = 1; i < num.length; i++) {
+            if (num[i] < min) min = num[i];
+            if (num[i] > max) max = num[i];
+        }
+
+        System.out.println("Minimum number of array is : " + min);
+        System.out.println("Maximum number of array is : " + max);
+    }
+}
