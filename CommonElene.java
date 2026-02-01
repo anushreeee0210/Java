608d5be49478b4acae57a4d40e7b56b8fdf04a10
anushree <anushreeee21@gmail.com>
2026-02-01 13:57:36 +0530

    Common element in 2 array

---
+++ CommonElene.java
@@ -0,0 +1,21 @@
+public class CommonElementin2 {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = {2, 3, 5, 6};
+
+        int i = 0, j = 0;
+
+        System.out.print("Common elements: ");
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] == arr2[j]) {
+                System.out.print(arr1[i] + " ");
+                i++;
+                j++;
+            } else if (arr1[i] < arr2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+    }
+}
