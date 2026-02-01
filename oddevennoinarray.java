6f75dd085bdc000ac650bed915e69708ad741404
anushree <anushreeee21@gmail.com>
2026-02-01 13:24:57 +0530

    odd even in a array

---
+++ oddevennoinarray.java
@@ -0,0 +1,19 @@
+public class oddevennoinarray {
+    public static void main(String[] args) {
+        int[] arr = {10, 02, 19, 25, 24, 16};
+
+        System.out.print("Even numbers: ");
+        for (int num : arr) {
+            if (num % 2 == 0) {
+                System.out.print(num + " ");
+            }
+        }
+
+        System.out.print("\nOdd numbers: ");
+        for (int num : arr) {
+            if (num % 2 != 0) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+}
