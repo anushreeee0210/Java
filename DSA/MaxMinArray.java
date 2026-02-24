65cb53c0059554bef914a580d30e864e75b8ff89
anushree <anushreeee21@gmail.com>
2026-02-24 20:33:51 +0530

    Max n Min array

---
+++ DSA/MaxMinArray.java
@@ -0,0 +1,19 @@
+public class MaxMinArray {
+    public static void main(String[] args) {
+        int[] numbers = {10, 56, 78, 34, 98, 23};
+        int max = numbers[0];
+        int min = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+
+        System.out.println("Maximum number: " + max);
+        System.out.println("Minimum number: " + min);
+    }
+}
