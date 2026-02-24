8566f263f647f9613ca010af482f7beb606f5914
anushree <anushreeee21@gmail.com>
2026-02-24 20:17:21 +0530

    Reversed Array

---
+++ DSA/reversearray.java
@@ -0,0 +1,39 @@
+public class reversearray {
+
+    public static void main(String[] args) {
+        
+        int[] arr = {1, 2, 3, 4, 5};
+
+        System.out.println("Original Array:");
+        printArray(arr);
+
+        reverseArray(arr);
+
+        System.out.println("Reversed Array:");
+        printArray(arr);
+    }
+
+    
+    public static void reverseArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            // Swap elements
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+
+    
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
