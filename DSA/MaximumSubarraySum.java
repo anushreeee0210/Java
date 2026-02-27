9d47f194da2f08a4204631f59876d28e0534e8a8
anushree <anushreeee21@gmail.com>
2026-02-27 20:06:29 +0530

    max subarray

---
+++ DSA/MaximumSubarraySum.java
@@ -0,0 +1,22 @@
+public class MaximumSubarraySum {
+
+    public static int maxSubArray(int[] arr) {
+        int maxSoFar = arr[0];
+        int currentMax = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            currentMax = Math.max(arr[i], currentMax + arr[i]);
+
+            maxSoFar = Math.max(maxSoFar, currentMax);
+        }
+
+        return maxSoFar;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+        int result = maxSubArray(arr);
+        System.out.println("Maximum Subarray Sum: " + result);
+    }
+}
